2c4094c082d83cc153b70c1db798b6118b9c1159
humraz <humrazsait@gmail.com>
2024-02-28 13:17:23 +0530

    Updates

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -35,33 +35,24 @@ function Subcard(props) {
             {showModal ? (
                 <>
                 <div
-                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                >
-                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                    className="justify-center items-center flex flex-col overflow-auto  fixed inset-0 z-50 outline-none focus:outline-none">
+                    <div className="w-auto mx-auto max-w-3xl">
                     {/*content*/}
-                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    <div className="border-1 rounded-lg shadow-lg relative  w-full bg-white outline-none focus:outline-none">
                         {/*header*/}
-                        <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                        <div className="flex p-5 border-b border-solid border-blueGray-200 rounded-t">
                         <h3 className="text-xl font-semibold">
                            {props.title}
                         </h3>
-                        <button
-                            className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                            onClick={() => setShowModal(false)}
-                        >
-                            <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                            ×
-                            </span>
-                        </button>
                         </div>
                         {/*body*/}
-                        <div className="relative p-6 flex-auto">
-                        <p className="my-4 text-blueGray-500 text-sm leading-relaxed">
+                        <div className="relative p-4 flex-auto">
+                        <p className="text-blueGray-500 md:text-sm text-xs overflow-auto max-h-96 leading-relaxed ">
                             {props.content}
                         </p>
                         </div>
                         {/*footer*/}
-                        <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                        <div className="flex items-center justify-end p-2 border-t border-solid border-blueGray-200 rounded-b">
                         <button
                             className="text-gray-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                             type="button"
